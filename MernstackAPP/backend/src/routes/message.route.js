b60495bc39835f96c6c18422d0b6d77612dc6309
shivani1668 <shivani.20244151@mnnit.ac.in>
2026-06-24 02:21:26 +0530

    Add story routes and delete message functionality

--- MernstackAPP/backend/src/routes/message.route.js
+++ MernstackAPP/backend/src/routes/message.route.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages, sendMessage, getStories, postStory, deleteMessages } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/stories/all", protectRoute, getStories);
 router.get("/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);
+router.post("/stories/post", protectRoute, postStory);
+
+router.delete("/delete/:id", protectRoute, deleteMessages);
 
 export default router;
