569e63bd79dd05d6bf763557e9803f57eee51d23
shivani1668 <shivani.20244151@mnnit.ac.in>
2026-06-24 09:06:04 +0530

    Add password reset routes to auth.route.js

--- MernstackAPP/backend/src/routes/auth.route.js
+++ MernstackAPP/backend/src/routes/auth.route.js
@@ -1,12 +1,18 @@
 import express from "express";
 import { signup, login, logout, updateProfile, checkAuth, deleteAccount, updatePassword } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { forgotPassword, resetPassword } from "../controllers/auth.controller.js";
+
+
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 router.put("/update-profile", protectRoute, updateProfile);
 router.put("/update-password", protectRoute, updatePassword);
