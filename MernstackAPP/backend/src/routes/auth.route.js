10e87c3c4d07d18f92c3275782939170da0ab47a
shivani1668 <shivani.20244151@mnnit.ac.in>
2026-06-24 12:44:12 +0530

    Update auth.route.js

--- MernstackAPP/backend/src/routes/auth.route.js
+++ MernstackAPP/backend/src/routes/auth.route.js
@@ -1,16 +1,12 @@
 import express from "express";
-import { signup, login, logout, updateProfile, checkAuth, deleteAccount, updatePassword } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile, checkAuth, deleteAccount, updatePassword, forgotPassword, resetPassword } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { forgotPassword, resetPassword } from "../controllers/auth.controller.js";
-
-
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 
